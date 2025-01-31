1a0b9ed0b6594d0d447a431f263f7fbcc6377edc
Beata <manska.b@gmail.com>
2025-01-31 20:46:33 +0100

    fix7

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,21 +2,23 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from './views/HomeView.vue';
 import UsersView from './views/UsersView.vue';
 import AboutView from './views/AboutView.vue';
-import UserDetails from './components/UserDetails.vue';
+import UserDetails from './views/UserDetails.vue';
+
+
 
 
 //Definicja tras
 const routes = [
-    { path: '/', component: Home, name: 'HomeView' },
-    { path: '/users', component: Users, name: 'UsersView' },
-    { path: '/users/:id', component: UserDetails }
-    { path: '/about', component: About, name: 'AboutView' },
+    { path: '/', component: HomeView },
+    { path: '/users', component: UsersView },
+    { path: '/users/:id', component: UserDetails },
+    { path: '/about', component: AboutView },
 ];
 
 //Tworzenie routera
 const router = createRouter({
     history: createWebHistory(),
-    routes,
+    routes
 });
 
 export default router;
