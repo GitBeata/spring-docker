73ace7e066c105407bbcb1328723f1b73d47d9ae
Beata <manska.b@gmail.com>
2025-01-31 20:40:28 +0100

    cors

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -6,12 +6,18 @@
 
 module.exports = {
   devServer: {
+    hot: false,
+    liveReload: false,
     proxy: {
-      'api': {//każde przekierowanie zaaczynające się od api będzie przekierowane      
-        target: 'http://localhost:8080', // docelowy backen działa na porcie 8080
+      '/api': { //każde żądanie zaczynającie się od /api będzie przekierowane
+        target: 'http://localhost:8080', //docelowy backend działa porcie 8080
         changeOrigin: true, //podmiana nagłówka origin w żądaniu
-        pathRewrite: { '^/api': '' }, // usuwamy /api  z url przed przekazaniem do backendu
+        pathRewrite: { '^/api': '' }, //usuwamy /api z url przed przekazaniem do backendu
+        ws: false //wyłącza websocket
       },
+    },
+    client: {
+      webSocketURL: 'auto://0.0.0.0/ws'
     }
   }
 }
