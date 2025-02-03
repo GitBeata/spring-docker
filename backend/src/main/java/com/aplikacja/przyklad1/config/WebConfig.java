b0309d04c98fdef98ca896a34fa59f6dd40bdc60
Beata <manska.b@gmail.com>
2025-02-03 18:38:55 +0100

    webc

--- backend/src/main/java/com/aplikacja/przyklad1/config/WebConfig.java
+++ backend/src/main/java/com/aplikacja/przyklad1/config/WebConfig.java
@@ -1,15 +1,12 @@
-package com.aplikacja.przyklad1.config;
-
-import org.springframework.web.servlet.config.annotation.CorsConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.CONTEXT.annotation.Configuration;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMapping(CorsRegistry registry) {
-        registry.addMapping("/**").AllowedOrigins("http://localhost:8080");
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:8080");
     }
-
 }
